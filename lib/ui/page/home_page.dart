cee3ae0257e7d806bc9be5db5e0fc327e8c9ed4e
she <jy6c9w08@gmail.com>
2021-02-25 12:28:49 +0800

    添加主页

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:sharemoe/controller/home_controller.dart';
+import 'package:sharemoe/ui/widget/nav_bar.dart';
 
 class HomePage extends GetView<HomePageController> {
+  final ScreenUtil screen = ScreenUtil();
+
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<HomePageController>(builder: (controller) {
+    return GetX<HomePageController>(builder: (_) {
       return Stack(
         children: [
           PageView.builder(
-              // controller: _.pageController,
+              controller: _.pageController,
               itemCount: 4,
-              // onPageChanged: (index) => _.pageIndex.value = index,
+              onPageChanged: (index) => _.pageIndex.value = index,
               itemBuilder: (context, index) {
-                return GetX<HomePageController>(
-                  builder: (_) {
-                    return _.getPageByIndex(index);
-                  },
-                );
+                return _.getPageByIndex(index);
               }),
+          AnimatedPositioned(
+              bottom: _.pageIndex.value == 0
+                  ? controller.navBarBottom.value
+                  : screen.setHeight(25.0),
+              left: controller.navBarLeft.value,
+              right: controller.navBarRight.value,
+              child: NavBar(),
+              duration: Duration(milliseconds: 400))
         ],
       );
     });
