2526932b7a8211b8c91c98ffd029c641c0faecd1
fxt0706 <fxt0706@gmail.com>
2021-06-26 01:44:28 +0800

    fix - init the color of image favorite

--- lib/controller/image_controller.dart
+++ lib/controller/image_controller.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+
 import 'package:sharemoe/basic/config/get_it_config.dart';
 import 'package:sharemoe/basic/config/hive_config.dart';
+import 'package:sharemoe/basic/config/logger_config.dart';
 import 'package:sharemoe/data/model/illust.dart';
 import 'package:sharemoe/data/repository/user_repository.dart';
+
 
 class ImageController extends GetxController with SingleGetTickerProviderMixin {
   // bool isLiked = false;
@@ -26,12 +30,18 @@ class ImageController extends GetxController with SingleGetTickerProviderMixin {
       vsync: this,
     );
     colorAnimationController = AnimationController(
-      duration: Duration(milliseconds: 110),
+      duration: Duration(milliseconds: 400),
       vsync: this,
     );
-    colorCurveAnimationController =
-        CurvedAnimation(parent: colorAnimationController, curve: Curves.elasticInOut);
+    colorCurveAnimationController = CurvedAnimation(
+        parent: colorAnimationController, curve: Curves.fastLinearToSlowEaseIn);
     colorAnimation = favoriteColor.animate(colorCurveAnimationController);
+
+    if (illust.isLiked == true) {
+      colorAnimationController.forward();
+      logger.i('init image color');
+    }
+
     super.onInit();
   }
 
