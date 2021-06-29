0530f788f3de712d7d2334a83e657fb8980e7fd7
OysterQAQ <oysterqaq@gmail.com>
2021-06-29 12:18:14 +0800

    clean&format

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,15 @@ import 'package:get/get.dart';
 import 'package:sharemoe/basic/config/get_it_config.dart';
 import 'package:sharemoe/bindings/home_binding.dart';
 import 'package:sharemoe/routes/app_pages.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'basic/service/user_service.dart';
 
 void main() async {
   configureDependencies().then((value) {
-   return init();
+    return init();
   }).whenComplete(() => runApp(MyApp()));
 }
 
-init() async {
-
-}
+//初始化备用
+init() async {}
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
