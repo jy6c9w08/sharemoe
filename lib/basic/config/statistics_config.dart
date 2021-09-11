e4e843838dff894521105cc446c8f9ddec23f0fe
fxt0706 <fxt0706@gmail.com>
2021-09-12 00:47:11 +0800

    fix - 移除了 instance 方法来解决编译错误问题

--- lib/basic/config/statistics_config.dart
+++ lib/basic/config/statistics_config.dart
@@ -11,15 +11,15 @@ import 'logger_config.dart';
 class StatisticsConfig {
   static Future<FlBaiduMobStat> initStatistics() async {
     logger.i("百度统计开始初始化");
-    final bool key = await FlBaiduMobStat.instance
+    final bool key = await FlBaiduMobStat()
         .setApiKey(androidKey: '1c62ad79da', iosKey: 'd7a60ad100');
     String channelName = 'flutter';
     if (Platform.isAndroid) channelName += '- Android';
     if (Platform.isIOS) channelName += '- IOS';
     // final bool channel =
-        await FlBaiduMobStat.instance.setAppChannel(channelName);
+        await FlBaiduMobStat().setAppChannel(channelName);
     logger.i("百度统计开始初始化成功：$key");
-    return FlBaiduMobStat.instance;
+    return FlBaiduMobStat();
   }
 }
 
